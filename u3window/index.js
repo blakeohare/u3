d5b390b269161507cb0ed5e55796865fd33eb8b4
Blake O'Hare <mspaintmaestro@gmail.com>
2020-09-16 19:08:19 -0700

    Hack out a prototype of the basic pipeline for game windows.

--- u3window/index.js
+++ u3window/index.js
@@ -1,31 +1,9 @@
-const { app, BrowserWindow } = require('electron')
-const { ipcMain } = require('electron')
+const { app } = require('electron');
+const tunnel = require('./rendertunnel.js');
+let testgame = require('./testgame.js');
 
-let sendToRenderer;
-
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 1000,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-
-  sendToRenderer = msg => {
-    win.webContents.send('rboundmsg', msg);
-  };
-
-  // and load the index.html of the app.
-  win.loadFile('render/index.html');
-}
-
-ipcMain.on('mboundmsg', (event, arg) => {
-    console.log(arg) // prints "ping"
-})
-
-setTimeout(() => {
-    sendToRenderer('hello from main.');
-}, 5000);
-
-app.whenReady().then(createWindow);
+// TODO: just wrap this in tunnel logic
+app.whenReady().then(() => {
+    tunnel.markAsReady();
+    testgame.run();
+});
--- u3window/render/messages.js
+++ u3window/render/messages.js
@@ -1,10 +1,21 @@
 const { ipcRenderer } = require('electron');
 
+window.registerMessageListener = null;
+window.sendMessage = null;
 (() => {
 
+    let listener = null;
+    window.registerMessageListener = f => {
+        listener = f;
+    };
+
     ipcRenderer.on('rboundmsg', (event, data) => {
-        console.log(data);
+        if (listener !== null) {
+            listener(data.type, data.value);
+        }
     });
 
-    ipcRenderer.send('mboundmsg', 'hello from renderer');
+    window.sendMessage = (type, value) => {
+        ipcRenderer.send('mboundmsg', { type, value });
+    };
 })();
